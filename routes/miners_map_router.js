b8408dfcb9314b6353b5eb8247ab09f894d4e33a
Isan-Rivkin <isanrivkin@gmail.com>
2018-02-07 19:57:28 +0200

    miners cords in place

---
+++ routes/miners_map_router.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var io = null;
+var request = require("request");
+var cheerio = require("cheerio");
+url ='http://www.ethernodes.org/network/1';
+
+
+
+function getMiners(callback){
+    request({
+        uri: url,
+    }, function(error, response, body) {
+        var parsedHtml = cheerio.load(body);
+        parsedHtml('script').get().forEach(element=>{
+            //console.log(element.attribs['graphData']);
+        })
+        //console.log(parsedHtml('script').get()[0].attribs['graphData']);
+        var scripts = parsedHtml('script').filter(function() {
+            return (parsedHtml(this).html().indexOf('var graphData =') > -1);
+        });
+        if (scripts.length === 1) {
+            var text = parsedHtml(scripts[0]).html();
+            var json = '{"cords":' +text.replace('var graphData = ',"") + '}';
+            var coordinates = JSON.parse(json)['cords'];
+            callback(coordinates);
+        }else{
+            callback(null);
+        }
+    });
+}
+
+module.exports = {
+    router:router,
+    setIO: function(iop){
+        io = iop;
+        io.on('connection',function(socket){
+            socket.on('get_miners',function(){
+                getMiners(minersList=>{
+                    io.emit('post_miners',minersList);
+                });
+            });
+        });
+    }
+};
+
+
