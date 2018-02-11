1abb9be7cd9002b9c62f5556376c0232916f6c20
Tomer <tomersap77@gmail.com>
2018-02-11 18:38:11 +0200

    radio button fixed

--- client/src/app/components/navbar/top-menu.config.ts
+++ client/src/app/components/navbar/top-menu.config.ts
@@ -3,6 +3,7 @@ import {HomeComponent} from "../home/home.component";
 import {Routes} from "@angular/router";
 import {MinerMapComponent} from "../miner-map/miner-map.component";
 import {EtherAboutComponent} from "../ether-about/ether-about.component";
+import {EntitiesComponent} from "app/components/entities/entities.component";
 
 /**
  * Created by Tomer on 10/02/2018.
@@ -23,7 +24,12 @@ export const routerConfig: Routes = [
   {
     path: 'map',
     component: MinerMapComponent
-  }, {
+  },
+  {
+    path: 'addresses',
+    component: EntitiesComponent
+  },
+  {
     path: 'ether_about',
     component: EtherAboutComponent
   },
